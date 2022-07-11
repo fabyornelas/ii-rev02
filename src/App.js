1b0374b3ae29a4343c56c5712d928bbbf8dac038
Xavier José Esparza García <xavier.j.esparza.garcia@gmail.com>
2022-07-10 22:09:07 -0500

    Agregando código Header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import Rectangle4 from "./components/Rectangle4"
 import Header from "./components/Header"
 
 function App() {
-  return <Rectangle4 />
-}
-
-function App() {
+  
   return <Header />
+  return <Rectangle4 />
+  
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Header from "./components/Header"
+import "../style-sheets/Header.css"
 
 export default function Header() {
   return (

72f3ff34aef7f1cc931b382ea4bc68602e4c8e9a
Xavier José Esparza García <xavier.j.esparza.garcia@gmail.com>
2022-07-10 21:52:15 -0500

    Subiendo Header

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from "react"
+import Header from "./components/Header"
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="group-486 flex-row-vcenter">
+        <div className="group-486 flex-row-vcenter">
+          <div className="logo-ynombre flex-row">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/qlm362laaz-2%3A386?alt=media&token=32cd4a71-cf2d-4f68-b520-c63260268f12"
+              alt="Not Found"
+              className="mask-group"
+            />
+            <p className="txt-067">Incubando Inversionistas</p>
+          </div>
+          <div className="group-178 flex-row-vend">
+            <div className="navbar flex-row">
+              <p className="txt-1023">Inicio</p>
+              <p className="txt-497">Nosotros</p>
+              <p className="txt-497">Servicios</p>
+              <p className="txt-497">Usuarios</p>
+              <div className="group-121 flex-row-vcenter">
+                <p className="txt-992">Más</p>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/qlm362laaz-2%3A395?alt=media&token=6fb8fe1d-600b-40c2-8fe2-2892e189125d"
+                  alt="Not Found"
+                  className="group"
+                />
+              </div>
+            </div>
+            <div className="rectangle-3" />
+            <div className="cont-ctanos">
+              <p className="txt-495">Contáctanos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

ae2f2dfe80a23de2a874575c10f83ddf528666a9
Xavier José Esparza García <xavier.j.esparza.garcia@gmail.com>
2022-07-10 21:51:10 -0500

    Subiendo código Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import React from "react"
-
 import Rectangle4 from "./components/Rectangle4"
+import Header from "./components/Header"
 
 function App() {
   return <Rectangle4 />
 }
 
+function App() {
+  return <Header />
+}
+
 export default App;
-
-
-
